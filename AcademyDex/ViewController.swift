008f78e531e32963c8b4cd9452444cb6c8074055
Ciro Amato <ciroamato@10-20-50-216.0200.fed.it.iosda.org>
2018-10-12 15:35:37 +0200

    Initial Commit

---
+++ AcademyDex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AcademyDex
+//
+//  Created by Ciro Amato on 12/10/2018.
+//  Copyright © 2018 Ciro Amato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
